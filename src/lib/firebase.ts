cd6195bfb6b77d5e2bf229343a3f58264b2bb0a6
Aannu Kant <aannukant@gmail.com>
2025-09-11 12:13:54 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  projectId: "studio-7106087596-31272",
+  appId: "1:481717540030:web:75d9e225ba58b04bfa3aef",
+  storageBucket: "studio-7106087596-31272.firebasestorage.app",
+  apiKey: "AIzaSyA58HyH9t03mDAlBUazs6VBCcnwCInvAOU",
+  authDomain: "studio-7106087596-31272.firebaseapp.com",
+  messagingSenderId: "481717540030",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
